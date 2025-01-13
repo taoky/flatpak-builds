809ee7b592fac32d302fd571034778ed0e6e9b62
taoky <me@taoky.moe>
2025-01-13 18:25:15 +0800

    Fix reg.conf persistence issue

--- com.todesk.ToDesk/todesk.sh
+++ com.todesk.ToDesk/todesk.sh
@@ -2,12 +2,15 @@
 
 set -ex
 
+# Make /etc/todesk/reg.conf persistent
+ln -s "$XDG_CONFIG_HOME" /etc/todesk
+
 # Cheating the process that the executable files are in FAKE_BIN_DIR
 FAKE_BIN_DIR="$XDG_CONFIG_HOME/bin"
 
 export TODESK_PACK_NAME=todesk
 export LIBVA_DRIVER_NAME=iHD
-export LIBVA_DRIVERS_PATH="$APP_HOME/todesk/bin"
+export LIBVA_DRIVERS_PATH="/app/todesk/bin"
 
 # Load the hack library.
 export LD_PRELOAD=/app/lib/libtodesk_fix.so
