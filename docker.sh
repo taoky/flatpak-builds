98fec0a6854fa3f23b862f552f9be79294b3ba5a
taoky <me@taoky.moe>
2025-09-28 00:41:20 +0800

    Fix CI

--- docker.sh
+++ docker.sh
@@ -14,6 +14,7 @@ $DOCKER build -t taoky/flatpak-builds .
 # Run the container to build bundles
 $DOCKER run --rm --cap-add CAP_SYS_ADMIN --cap-add CAP_NET_ADMIN \
   --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+  --security-opt systempaths=unconfined \
   --device /dev/fuse --network=host \
   -v $(pwd):/workspace -e TODESK_DOMAIN=$TODESK_DOMAIN -e TODESK_DIR=$TODESK_DIR \
   -v flatpak-builds:/var/lib/flatpak/ \
